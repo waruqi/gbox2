70069bed438f0a9bbe521d2815a8086206a271ad
ruki <waruqi@gmail.com>
2012-10-12 07:55:01 +0000

    modify version

--- src/prefix/version.h
+++ src/prefix/version.h
@@ -32,10 +32,10 @@
  * macros
  */
 
-// version: v1.0.0
+// version: v1.0.3
 #define G2_VERSION_MAJOR 			(1)
 #define G2_VERSION_MINOR 			(0)
-#define G2_VERSION_ALTER 			(0)
+#define G2_VERSION_ALTER 			(3)
 
 #endif
 
